4a5846fa0f0b89e26d3de61c67a558317c77dee9
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-03-01 21:28:45 -0300

    Resilence4j fault tolerance

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -10,18 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.services.PaymentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
+	
+	private static final String PAYMENT_FALLBACK = "paymentFallback";
 	
 	@Autowired
 	private PaymentService service;
 	
 	@GetMapping(value = "/{workerId}/days/{days}")
+	@CircuitBreaker(name = PAYMENT_FALLBACK, fallbackMethod = "getPaymentAlternative") // caso aconteca alguma falha nesse endpoint
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
 	
+	// vai chamar esse metodo
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer dias, Throwable t) {
+		Payment payment = new Payment("Bran", 400.0, dias); // mock data
+		return ResponseEntity.ok(payment); // que retornara esse dado mockado
+	}
 
 }
